2260478c22438113ba122121529617e5d78632e9
ASHISH BASTOLA <ashishbastola@ASHISHs-MacBook-Pro.local>
2019-01-29 18:03:21 +0530

    first test

---
+++ test.py
@@ -0,0 +1,74 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS = 66, 200, 3
+# IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS = 160, 320, 1
+INPUT_SHAPE = (IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS)
+
+def crop(image):
+    """
+    Crop the image (removing the sky at the top and the car front at the bottom)
+    """
+    return image[60:-25, :, :] # remove the sky and the car front
+
+
+def resize(image):
+    """
+    Resize the image to the input shape used by the network model
+    """
+    return cv2.resize(image, (IMAGE_WIDTH, IMAGE_HEIGHT), cv2.INTER_AREA)
+
+
+def rgb2yuv(image):
+    """
+    Convert the image from RGB to YUV (This is what the NVIDIA model does)
+    """
+    return cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
+
+
+def preprocess(image):
+    """
+    Combine all preprocess functions into one
+    """
+    image = crop(image)
+    image = resize(image)
+    image = rgb2yuv(image)
+    return image
+
+def random_shadow(image):
+    """
+    Generates and adds random shadow
+    """
+    image = preprocess(image)
+    print(image.shape)
+    # (x1, y1) and (x2, y2) forms a line
+    # xm, ym gives all the locations of the image
+    x1, y1 = IMAGE_WIDTH * np.random.rand(), 0
+    x2, y2 = IMAGE_WIDTH * np.random.rand(), IMAGE_HEIGHT
+    xm, ym = np.mgrid[0:IMAGE_HEIGHT, 0:IMAGE_WIDTH]
+
+    # mathematically speaking, we want to set 1 below the line and zero otherwise
+    # Our coordinate is up side down.  So, the above the line: 
+    # (ym-y1)/(xm-x1) > (y2-y1)/(x2-x1)
+    # as x2 == x1 causes zero-division problem, we'll write it in the below form:
+    # (ym-y1)*(x2-x1) - (y2-y1)*(xm-x1) > 0
+
+    mask = np.zeros_like(resize(image)[:, :, 1])
+    mask[(ym - y1) * (x2 - x1) - (y2 - y1) * (xm - x1) > 0] = 1
+
+    # choose which side should have shadow and adjust saturation
+    cond = mask == np.random.randint(2)
+    s_ratio = np.random.uniform(low=0.2, high=0.5)
+
+    # adjust Saturation in HLS(Hue, Light, Saturation)
+    hls = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
+    hls[:, :, 1][cond] = hls[:, :, 1][cond] * s_ratio
+    return cv2.cvtColor(hls, cv2.COLOR_HLS2RGB)
+
+
+img_path = '/Users/ashishbastola/Desktop/college_final_year_project/project_1/IMG/center_2019_01_27_12_05_21_978.jpg'
+
+img = cv2.imread(img_path)
+plt.imshow(random_shadow(img))
+plt.show()
